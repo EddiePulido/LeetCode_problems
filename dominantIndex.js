c55f7dfde79115f0ebeedf42522d169052eeefc0
Eddie <eddiepulidodev@gmail.com>
2023-05-19 21:26:04 -0700

    May 19th

--- dominantIndex.js
+++ dominantIndex.js
@@ -5,3 +5,22 @@ const dominantIndex = (nums) => {
 
   return max2 * 2 <= max ? i : -1
 }
+
+const dominantIndex = nums => {
+  let max1 = Math.max(nums[0], nums[1])
+  let max2 = Math.min(nums[0], nums[1])
+  let index = nums.findIndex(e => e === max1)
+
+  for(let i = 2; i < nums.length; i++){
+      const num = nums[i]
+      if(num > max1){
+          max2 = max1
+          max1 = num
+          index = i
+      }else if(num > max2){
+          max2 = num
+      }
+  }
+
+  return max1 >= max2 * 2 ? index : -1
+}
