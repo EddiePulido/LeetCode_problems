a2bbc7d1bb27ff8bd575bba521d8e87f0d52b1e2
Eddie <eddiepulidodev@gmail.com>
2023-06-12 21:42:08 -0700

    June 12th

--- reverseOnlyLetter.js
+++ reverseOnlyLetter.js
@@ -1,15 +1,6 @@
 const reverseOnlyLetters = s => {
   const isChar = c => c.toLowerCase() !== c.toUpperCase()
 
-  let chars = [...s].filter(e => isChar(e)).reverse()
-  let i = 0
-  return [...s].map(e => isChar(e) ? chars[i++] : e).join('')
-}
-
-// two pointers
-const reverseOnlyLetters = s => {
-  const isChar = c => c.toLowerCase() !== c.toUpperCase()
-
   const arr = s.split('')
 
   let left = 0
